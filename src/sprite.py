8e95b50c25e8ad0f0113c1909be7d0ae4f86eff0
George <georgesut@yahoo.com>
2017-11-12 20:28:57 +0200

    Updated sprite.py

--- src/sprite.py
+++ src/sprite.py
@@ -1,16 +1,27 @@
 import pygame
 
 class Sprite:
-	_speed = 5
+	speed = 5
 
-	def __init__(self, x, y, image_path):
-		self.x = x
-		self.y = y
+	def __init__(self, image_path):
+		self.x = 0
+		self.y = 0
 		self.image = pygame.image.load(image_path)
+		self.height = self.image.get_height()
+		self.width = self.image.get_width()
 
 	def render(self, dest_surface):
 		dest_surface.blit(self.image, (self.x, self.y))
 
+	def set_pos(self, x, y):
+		self.x = x
+		self.y = y 
+
 	def move(self, offset_x, offset_y):
-		self.x += offset_x
-		self.y += offset_y
+		self.set_pos(self.x + offset_x, self.y + offset_y)
+
+	def get_width(self):
+		return self.width
+
+	def get_height(self):
+		return self.height
