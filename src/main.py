dea4c33fc7a9aa906830c39b151a9dffcac74b88
George <georgesut@yahoo.com>
2017-11-12 22:00:01 +0200

    Added FPS

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 import pygame
 from player import Player
+from ball import Ball
+from settings import *
 pygame.init()
-
-window_width = 800
-window_height = 600
-dist_from_edge = 20
 
 screen = pygame.display.set_mode((window_width, window_height))
 running = True
@@ -13,6 +11,11 @@ player1 = Player()
 player2 = Player()
 player1.set_pos(dist_from_edge, window_height / 2 - player1.get_height() / 2)
 player2.set_pos(window_width - player2.get_width() - dist_from_edge, window_height / 2 - player2.get_height() / 2)
+
+ball = Ball()
+ball.reset()
+
+start_time = pygame.time.get_ticks()
 
 while running:
 	for event in pygame.event.get():
@@ -30,9 +33,13 @@ while running:
 	if pressed_keys[pygame.K_DOWN]:
 		player2.move(0, Player.speed)
 
-	screen.fill((0, 0, 0))
-	player1.render(screen)
-	player2.render(screen)
-	pygame.display.flip()
+	current_time = pygame.time.get_ticks()
+	if current_time - start_time > 1000/FPS:
+		start_time = pygame.time.get_ticks()
+		screen.fill((0, 0, 0))
+		player1.render(screen)
+		player2.render(screen)
+		ball.render(screen)
+		pygame.display.flip()
 
 pygame.quit()
