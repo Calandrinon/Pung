f627ead72f62b5ba01d36691014ce5419f3e717b
George Sut <georgesut@yahoo.com>
2017-11-18 13:09:44 +0200

    Added score class

---
+++ src/score.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Score:
+    def __init__(self, x, y, font, color):
+        self.amount = 0
+        self.x = x
+        self.y = y
+        self.font = font
+        self.color = color
+        self.score_font = pygame.font.SysFont(font, 30)
+
+    def increase(self):
+        self.amount += 1
+
+    def render(self, dest_surface):
+        text = self.score_font.render(str(self.amount), False, self.color)
+        dest_surface.blit(text, (self.x, self.y))
