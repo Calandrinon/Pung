aeae027b86f853276d07b8f44265023780b2cc53
George Sut <georgesut@yahoo.com>
2017-11-18 10:58:00 +0200

    Changed ball speed

--- src/ball.py
+++ src/ball.py
@@ -4,6 +4,8 @@ from sprite import *
 from settings import *
 
 class Ball(Sprite):
+	sign_x = 1
+
 	def __init__(self):
 		super(Ball, self).__init__("../res/ball.jpg")
 		self.start_x = window_width / 2 - self.get_width() / 2
@@ -11,15 +13,18 @@ class Ball(Sprite):
 		self.finish_x = None
 		self.finish_y = None
 		self.set_pos(self.start_x, self.start_y)
+		self.speed = 1
 
 	def reset(self):
 		if random.randint(0, 2) == 1:
 			self.finish_x = window_width
+			self.sign_x = 1
 		else:
 			self.finish_x = 0
+			self.sign_x = -1
 
 		finish_y_option1 = random.randint(unit, window_height / 2 - unit)
-		finish_y_option2 = random.randint(window_height / 2 + unit, window_height - unit) 
+		finish_y_option2 = random.randint(window_height / 2 + unit, window_height - unit)
 		if random.randint(0, 2) == 1:
 			self.finish_y = finish_y_option1
 		else:
@@ -29,3 +34,6 @@ class Ball(Sprite):
 		self.f = lambda x: self.slope * (x - self.finish_x) + self.finish_y
 		print(self.finish_x, self.finish_y)
 		print(self.start_x, self.start_y)
+
+	def move_it(self):
+		super(Ball, self).set_pos(self.x + self.sign_x * self.speed, self.f(self.x + self.sign_x * self.speed))
