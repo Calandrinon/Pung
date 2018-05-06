d14a6298205b257fdcbb5795f0eaba6f202f554c
George <georgesut@yahoo.com>
2018-05-06 15:35:59 +0300

    Shorten main_loop function(for readability purposes)

--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@ from settings import *
 def init():
 	os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (20,20)
 	pygame.init()
-	global screen, player1, player2, ball
+	global screen, player1, player2, ball, start_time, win_clock
 	screen = pygame.display.set_mode((window_width, window_height), pygame.FULLSCREEN)
+	start_time = win_clock = pygame.time.get_ticks()
 	pygame.display.set_caption("Pung")
-	
+
 	player1 = Player()
 	player2 = Player()
 	ball = Ball()
@@ -44,11 +45,63 @@ def handle_input():
 
 	return False
 
+def has_won():
+	won1 = player1.won()
+	won2 = player2.won()
+
+	if won1 or won2:
+		font_size = 50
+		font_type = "Times New Roman"
+		font = pygame.font.SysFont(font_type, font_size)
+
+		if won1:
+			message = "Player 1 won!"
+		else:
+			message = "Player 2 won!"
+
+		game_over_msg_size = 13
+		esc_msg_size = 17
+		game_over = font.render(message, False, (5, 165, 62))
+		esc = font.render("Press ESC to exit", False, (255, 0, 0))
+		game_over_pos = (window_width / 2 - (game_over_msg_size * font_size) / 2, window_height / 2 - font_size / 2)
+		esc_pos = (window_width / 2 - (esc_msg_size * font_size) / 2, window_height - window_height / 5)
+
+		screen.blit(game_over, game_over_pos)
+		screen.blit(esc, esc_pos)
+		pygame.display.update()
+		return True
+	return False
+
+def draw():
+	global start_time, win_clock
+
+	current_time = pygame.time.get_ticks()
+	if current_time - start_time > 1000 / FPS:
+		start_time = pygame.time.get_ticks()
+		winner = ball.out_of_boundaries(player1, player2)
+		if winner[0]:
+			win_clock = pygame.time.get_ticks()
+			ball.reset(winner[1])
+
+		screen.fill((0, 0, 0))
+
+		current_time = pygame.time.get_ticks()
+		if current_time - win_clock > 2000:
+			ball.move()
+			ball.bounce(player1, player2)
+			ball.render(screen)
+
+		player1.render(screen)
+		player2.render(screen)
+		player1.render_score(screen)
+		player2.render_score(screen)
+		pygame.draw.line(screen, (255, 255, 255), (window_width / 2, 0), (window_width / 2, window_height))
+		pygame.display.update()
+
 def main_loop():
-	start_time = pygame.time.get_ticks()
 	running = True
 	game_is_over = False
-	win_clock = pygame.time.get_ticks()
+
 	while running:
 		if handle_input():
 			running = False
@@ -56,44 +109,8 @@ def main_loop():
 		if game_is_over:
 			continue
 
-		current_time = pygame.time.get_ticks()
-		if current_time - start_time > 1000/FPS:
-			start_time = pygame.time.get_ticks()
-			winner = ball.out_of_boundaries(player1, player2)
-			if winner[0]:
-				win_clock = pygame.time.get_ticks()
-				ball.reset(winner[1])
-			
-			screen.fill((0, 0, 0))
-			
-			current_time = pygame.time.get_ticks()
-			if current_time - win_clock > 2000:
-				ball.move()
-				ball.bounce(player1, player2)
-				ball.render(screen)
-
-			player1.render(screen)
-			player2.render(screen)
-			player1.render_score(screen)
-			player2.render_score(screen)
-			pygame.draw.line(screen, (255, 255, 255), (window_width / 2, 0), (window_width/2, window_height))
-
-			if player1.won():
-				font = pygame.font.SysFont("Times New Roman", 50)
-				game_over = font.render("Player 1 won!", False, (5, 165, 62))
-				screen.blit(game_over, (window_width / 2 - 150, window_height / 2 - 50 * 2))
-				screen.blit(font.render("Press ESC to exit", False, (255, 0, 0)), (window_width / 2 - 150, window_height / 2))
-				game_is_over = True
-
-
-			elif player2.won():
-				font = pygame.font.SysFont("Times New Roman", 50)
-				game_over = font.render("Player 2 won!", False, (5, 165, 62))
-				screen.blit(game_over, (window_width / 2 - 150, window_height / 2 - 50 * 2))
-				screen.blit(font.render("Press ESC to exit", False, (255, 0, 0)), (window_width / 2 - 150, window_height / 2));
-				game_is_over = True
-
-			pygame.display.flip()
+		game_is_over = has_won()
+		draw()
 
 	pygame.quit()
 
